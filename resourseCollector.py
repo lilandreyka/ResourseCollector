134a8a30c3d50619918af7ef0679c4ab33af1dc2
killerthebes <kolyvanov-03@mail.ru>
2019-08-20 11:47:30 +0700

    базовый функционал робота -движение -отрисовка

--- resourseCollector.py
+++ resourseCollector.py
@@ -1,24 +1,47 @@
 import arcade
 import random
+from math import sin, cos, radians
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 
 class Robot:
-    def __init__(self):
-        self.x = random.randint(0, SCREEN_WIDTH)
-        self.y = random.randint(0, SCREEN_HEIGHT)
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
         self.dx = 0
         self.dy = 0
+        self.speed = 0
+        self.ang = 0
         self.size = 40
         self.color = arcade.color.BLOND
 
+    def turn_left(self):
+        self.ang -= 10
+        self.dx = sin(radians(self.ang))
+        self.dy = cos(radians(self.ang))
+
+    def turn_right(self):
+        self.ang += 10
+        self.dx = sin(radians(self.ang))
+        self.dy = cos(radians(self.ang))
+
+    def speed_up(self):
+        if self.speed < 5:
+            self.speed += 1
+
+    def speed_down(self):
+        if self.speed >= 0:
+            self.speed -= 1
+
     def move(self):
-        self.x += self.dx
-        self.y += self.dy
+        self.x += self.dx * self.speed
+        self.y += self.dy * self.speed
 
     def draw(self):
+        arcade.draw_rectangle_filled(self.x, self.y, self.size, self.size,
+                                     [100, 0, 0])
         pass
 
 
@@ -39,22 +62,29 @@ class MyGame(arcade.Window):
 
     def setup(self):
         # Настроить игру здесь
+        self.robot = Robot(100, 250)
         pass
 
     def on_draw(self):
         """ Отрендерить этот экран. """
         arcade.start_render()
         # Здесь код рисунка
+        self.robot.draw()
 
     def on_key_press(self, symbol: int, modifiers: int):
         if symbol == arcade.key.LEFT:
-             pass
+             self.robot.turn_left()
         if symbol == arcade.key.RIGHT:
-            pass
+             self.robot.turn_right()
+        if symbol == arcade.key.UP:
+             self.robot.speed_up()
+        if symbol == arcade.key.DOWN:
+             self.robot.speed_down()
 
 
     def update(self, delta_time):
         """ Здесь вся игровая логика и логика перемещения."""
+        self.robot.move()
         pass
 
 
