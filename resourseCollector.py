b33568f6bdfde185ce36161ccf23a9184f6a9480
Roman <you@example.com>
2019-08-20 12:10:38 +0700

    Merge branch 'master' of C:\PycharmProjects\ResourseCollector with conflicts.

--- resourseCollector.py
+++ resourseCollector.py
@@ -1,53 +1,30 @@
 import arcade
 import random
-from math import sin, cos, radians
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 
 class Robot:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self):
+        self.x = random.randint(0, SCREEN_WIDTH)
+        self.y = random.randint(0, SCREEN_HEIGHT)
         self.dx = 0
         self.dy = 0
-        self.speed = 0
-        self.ang = 0
         self.size = 40
         self.color = arcade.color.BLOND
 
-    def turn_left(self):
-        self.ang -= 10
-        self.dx = sin(radians(self.ang))
-        self.dy = cos(radians(self.ang))
-
-    def turn_right(self):
-        self.ang += 10
-        self.dx = sin(radians(self.ang))
-        self.dy = cos(radians(self.ang))
-
-    def speed_up(self):
-        if self.speed < 5:
-            self.speed += 1
-
-    def speed_down(self):
-        if self.speed >= 0:
-            self.speed -= 1
-
     def move(self):
-        self.x += self.dx * self.speed
-        self.y += self.dy * self.speed
+        self.x += self.dx
+        self.y += self.dy
 
     def draw(self):
-        arcade.draw_rectangle_filled(self.x, self.y, self.size, self.size,
-                                     [100, 0, 0])
         pass
 
 
 class Background:
     def __init__(self):
-        self.img = arcade.load_texture("img/6561457.gif")
+        self.img = arcade.load_texture("img/iVCFFHH8S.jpg")
 
     def draw(self):
         arcade.draw_texture_rectangle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT, self.img)
@@ -62,31 +39,25 @@ class MyGame(arcade.Window):
 
     def setup(self):
         # Настроить игру здесь
-        self.robot = Robot(100, 250)
+        backgroud = Background()
         pass
 
     def on_draw(self):
         """ Отрендерить этот экран. """
         arcade.start_render()
+        arcade.draw_texture_rectangle()
         # Здесь код рисунка
-        self.robot.draw()
 
     def on_key_press(self, symbol: int, modifiers: int):
         if symbol == arcade.key.LEFT:
-             self.robot.turn_left()
+             pass
         if symbol == arcade.key.RIGHT:
-             self.robot.turn_right()
-        if symbol == arcade.key.UP:
-             self.robot.speed_up()
-        if symbol == arcade.key.DOWN:
-             self.robot.speed_down()
+            pass
 
-
+    main()
     def update(self, delta_time):
         """ Здесь вся игровая логика и логика перемещения."""
-        self.robot.move()
         pass
-
 
 def main():
     game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
@@ -95,4 +66,3 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
