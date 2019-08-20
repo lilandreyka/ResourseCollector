cac416191f4108d9b5cb7d803bd8981b9ed9b2e6
user <gluten>
2019-08-20 12:08:17 +0700

    Добавлен базовый ресурс

--- resourseCollector.py
+++ resourseCollector.py
@@ -4,7 +4,7 @@ from math import sin, cos, radians
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-
+RESOURSE_COUNT = 4
 
 class Robot:
     def __init__(self, x, y):
@@ -15,7 +15,7 @@ class Robot:
         self.speed = 0
         self.ang = 0
         self.size = 40
-        self.color = arcade.color.BLOND
+        self.color = [150, 150, 150]
 
     def turn_left(self):
         self.ang -= 10
@@ -40,9 +40,18 @@ class Robot:
         self.y += self.dy * self.speed
 
     def draw(self):
-        arcade.draw_rectangle_filled(self.x, self.y, self.size, self.size,
-                                     [100, 0, 0])
+        arcade.draw_rectangle_filled(self.x, self.y, self.size, self.size, [100, 0, 0])
         pass
+
+class Resurse:
+    def __init__(self):
+        self.size = 50
+        self.x = random.randint(self.size, SCREEN_WIDTH - self.size)
+        self.y = random.randint(self.size, SCREEN_WIDTH - self.size)
+        self.color = [150, 150, 100]
+
+    def draw(self):
+        arcade.draw_rectangle_filled(self.x, self.y, self.size, self.size, self.color)
 
 
 class Background:
@@ -57,18 +66,23 @@ class MyGame(arcade.Window):
 
     def __init__(self, width, height):
         super().__init__(width, height)
-
         arcade.set_background_color(arcade.color.AMAZON)
+        self.resurse_list = []
 
     def setup(self):
         # Настроить игру здесь
         self.robot = Robot(100, 250)
+        for i in range(RESOURSE_COUNT):
+            self.resurse_list.append(Resurse())
         pass
 
     def on_draw(self):
         """ Отрендерить этот экран. """
         arcade.start_render()
         # Здесь код рисунка
+        for resourse in self.resurse_list:
+            resourse.draw()
+
         self.robot.draw()
 
     def on_key_press(self, symbol: int, modifiers: int):
